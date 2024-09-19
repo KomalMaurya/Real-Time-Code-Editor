2cab29a617013d2d652c37bf2a977b102bef2d9e
Komal Maurya <145214525+KomalMaurya@users.noreply.github.com>
2024-09-19 08:34:42 +0530

    Repo Done

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+require('./Models/db');
+const cors=require('cors');
+const bodyParser = require('body-parser');
+const AuthRouter = require('./Routes/AuthRouter');
+const PORT = process.env.PORT || 8080;
+
+app.get('/ping',(req,res)=>{
+    res.send("komal");
+})
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth',AuthRouter);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
