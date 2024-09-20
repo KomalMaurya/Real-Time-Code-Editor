b0c6b8d9e12e833773189dab771619730c0785b1
Komal Maurya <145214525+KomalMaurya@users.noreply.github.com>
2024-09-20 08:39:47 +0530

    Frontend Basics Ready

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './login.css';
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+    <div className='login-page'>
+      <h1 style={{textAlign:"center"}}>Login</h1>
+        <div className='form'>
+          <form className='login-form'>
+            <input type='text' name='' id='' placeholder='Username'/>
+            <input type='password' name='' id='' placeholder='password'/>
+            <button>Login</button>
+            <p className='message'>Not Registered? <a href='#_'>Create an account </a></p>
+          </form>
+          <button className='login-with-google-btn'>
+            Sign with Google
+          </button>
+        </div>
+
+    </div>
+    </>
   )
 }
 
